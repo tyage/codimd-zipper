cca623d03bf85a792d2ab3739063ff081594fd84
tyage <namatyage@gmail.com>
2022-12-22 15:55:57 +0900

    download document, parse image

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,22 @@
 import browser from 'webextension-polyfill'
-import { downloadDocument } from "./downloader";
+import downloadDocument from './downloader?script';
 
 browser.contextMenus.create(
   {
     id: "download-zip",
     title: "Zip CodiMD",
     contexts: ["all"],
-  },
-  downloadDocument
+  }
 );
+browser.contextMenus.onClicked.addListener((_, tab) => {
+  if (tab?.id === undefined) {
+    return
+  }
 
+  // XXX: couldn't execute script multiple times because of import... how we can fix it?
+  browser.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: [downloadDocument]
+  });
+
+})
--- src/downloader.ts
+++ src/downloader.ts
@@ -1,2 +1,40 @@
-export function downloadDocument() {
+async function fetchDocument() {
+  const url = location.pathname + '/download';
+  return (await fetch(url)).text()
 }
+
+function getDocumentImages(document: string): string[] {
+  const imagePattern = /\!\[([^\]]*)\]\(([^)]*)\)/g
+  const images = []
+  for (let match of document.matchAll(imagePattern)) {
+    const url = match[2]
+    images.push(url)
+  }
+  return images
+}
+
+async function fetchImages(imageURLs: string[]) {
+  console.log(imageURLs)
+  throw new Error("Function not implemented.");
+}
+
+async function makeZip(document: string, images: void) {
+  throw new Error("Function not implemented.");
+}
+
+function downloadZip(zip: void) {
+  throw new Error("Function not implemented.");
+}
+
+async function main() {
+  const document = await fetchDocument()
+  const imageURLs = getDocumentImages(document)
+  const images = await fetchImages(imageURLs)
+  const zip = await makeZip(document, images)
+  downloadZip(zip)
+}
+
+main()
+
+// To supress isolatedModules error
+export {}
